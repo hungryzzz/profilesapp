f04831b9c2c1c37cfb39112239be0d4092df45f9
hungryzzz <512660048@qq.com>
2025-04-27 16:43:41 +0800

    modify dependency

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,6 +1,5 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 import { postConfirmation } from '../auth/post-confirmation/resource';
-import { post } from 'aws-amplify/api';
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
